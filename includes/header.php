04b1c7623abf7b02386330befc821faef7d79236
Cleiton Almeida <120829611+K3uzin@users.noreply.github.com>
2023-06-15 08:34:19 -0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style-header.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <a href="../pages/index.php">
+        <img src="../img/logo.png" alt="Logo Cidade do Saber">
+      </a>
+    </div>
+    <div class="header-content">
+      <a href="../pages/index.php" class="projeto">Projeto Cidade do Saber</a>
+    </div>
+    <div class="ajuda"><a href="../pages/ajuda.php">Ajuda</a></div>
+  </header>
+</body>
+
+</html>
