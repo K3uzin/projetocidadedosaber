a96eb86024f3493a8d4ca074ed69274fb509ad3e
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-06-20 21:14:49 -0300

    Update turma.class.php

--- classes/turma.class.php
+++ classes/turma.class.php
@@ -3,22 +3,22 @@
    class Turma{
         
     
-        private $nome
-        private $id_turma
-        private $senhas_disponoveis
-        private $Status
+        private $nome;
+        private $id_turma;
+        private $senhas_disponoveis;
+        private $Status;
         
         public function set_turma_status($id_turma,$status){
 
             if($status == true){
                 $this->status = true;
-                $stmt = $conexao->prepare("UPDATE turma SET turma_avability = $this->status WHERE cod_turma=$id_turma")
+                $stmt = $conexao->prepare("UPDATE turma SET turma_avability = $this->status WHERE cod_turma=$id_turma");
                 $stmt->bind_param("b", $this->status);
                 $stmt->execute();
             }
             if($status == false){
                 $this->status = false;
-                $stmt = $conexao->prepare("UPDATE turma SET turma_avability = $this->status WHERE cod_turma=$id_turma"))
+                $stmt = $conexao->prepare("UPDATE turma SET turma_avability = $this->status WHERE cod_turma=$id_turma");
                 $stmt->bind_param("b", $this->status);
                 $stmt->execute();
             }
