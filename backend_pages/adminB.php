9575999240447389963b0225e617f181d909d2f6
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-06-27 12:51:12 -0300

    Update adminB.php

--- backend_pages/adminB.php
+++ backend_pages/adminB.php
@@ -1,39 +1,35 @@
 <?php
-
 require_once("../Database/conexao.php");
 require_once("../classes/adm.class.php");
 
-if(!isset($_POST)){
+if (!isset($_POST)) {
     header("../pages/admin.php");
-}else{
-
+} else {
     $Adm = $_POST["nome"];
     $Adm_nome = $_POST["nome"];
     $Adm_senha = $_POST["senha"];
 
-    $query = ("SELECT nome, senha FROM adm WHERE nome= '$Adm_nome';");
-    $result = mysqli_query($query,$conexao);
+    $query = "SELECT nome, senha FROM adm WHERE nome = '$Adm_nome';";
+    $result = mysqli_query($conexao, $query);
     $row = mysqli_fetch_assoc($result);
     $rows_num = mysqli_num_rows($result);
 
-    if($rows_num == 0){
-        
-        exit("nome ou usuario errado");
-
-    }else{
-        
-        $veri_senha = password_verify($Adm_senha,$row["senha"]);
-        if(!$veri_senha){
-
-            exit("senha errada");     
-        }else{
-
+    if ($rows_num == 0) {
+        echo "<p style='color: red;'>Nome de usuário incorreto</p>";
+        exit();
+    } else {
+        $veri_senha = password_verify($Adm_senha, $row["senha"]);
+        if (!$veri_senha) {
+            echo "<p style='color: red;'>Senha incorreta</p>";
+            exit();
+        } else {
             $Adm = new Adm;
-            $Adm->get_Adm_id($Adm,$Adm_nome);
+            $Adm->get_Adm_id($Adm, $Adm_nome);
             $Adm->get_Adm_att($Adm);
             session_start();
             $_SESSION["Adm"] = $Adm;
-        header(/*header para a home da Adm*/);
+            header(/*header para a home da Adm*/);
         }
-    }        
+    }
 }
+?>
