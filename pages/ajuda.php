200f9006fc8d7c5a378e8b647a77c10e93af649f
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-06-27 21:23:27 -0300

    Update ajuda.php

--- pages/ajuda.php
+++ pages/ajuda.php
@@ -4,12 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style-ajuda.css">
     <title>Minha Página</title>
 </head>
 
 <body>
     <?php require '../includes/header.php'; ?>
+    <h1> Como podemos ajudar ? </h1>
 
+    <div>
+        <button> Ajuda com inscrições </button>
+    </div>
+
+    <div>
+        <button> Ajuda com cadastro </button>
+    </div>
     <!-- O restante do conteúdo da página -->
 
 </body>
