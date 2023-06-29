dba90b5151a8ae59ba93d0391ac44fef806dc588
Jfilho04 <129802247+Jfilho04@users.noreply.github.com>
2023-06-28 21:23:43 -0300

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style-index.css">
     <title>Minha Página</title>
 </head>
 
@@ -11,6 +12,153 @@
     <?php require '../includes/header.php'; ?>
 
     <!-- O restante do conteúdo da página -->
+
+<div>
+    <table class="c1">
+
+        <thead>
+            <br> <br> <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Bateria <br> A partir de 12 anos</th>     
+            </tr>
+
+        </thead>
+
+    <table class="c2">
+        
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Teclado <br> De 12 a 40 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c3">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Violão <br> De 12 a 40 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c4">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Canto <br>  A partir de 10 anos</th>     
+            </tr>
+
+        </thead>
+        <table class="c5">
+        
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Teatro <br> De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c6">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Flauta <br> De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c7">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Violino <br>  De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+
+</div>
+
+
+<div class="p1">
+    
+    <table class="c8">
+
+        <thead>
+            <br> <br> <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Viola <br> De 07 a 30 anos</th>
+                
+            </tr>
+
+        </thead>
+
+    <table class="c9">
+        
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Trompa <br> De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c10">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Trompete <br> De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c11">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Saxofone <br>  De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+        <table class="c12">
+        
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Contrabaixo <br> De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c13">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Violoncelo <br> De 07 a 30 anos</th>     
+            </tr>
+
+        </thead>
+
+        <table class="c14">
+
+        <thead>
+            <br> <br> 
+            <tr> 
+                <th> <a href="inscricao.php"> Ballet <br> A partir de 06 anos</th>     
+            </tr>
+
+        </thead>
+
+</div>
 
 </body>
 
