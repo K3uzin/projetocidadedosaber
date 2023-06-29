7e533396a4b5ad443f4ce739685427ebd47a12ee
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-06-28 21:18:10 -0300

    Update ajuda.php

--- pages/ajuda.php
+++ pages/ajuda.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -10,16 +10,34 @@
 
 <body>
     <?php require '../includes/header.php'; ?>
-    <h1> Como podemos ajudar ? </h1>
+    <h1> Como podemos ajudar? </h1>
 
     <div class="container">
         <ul>
-             <li class="lista" > Qual tempo de validade da senha ?</li>
-             <li class="lista" > Tirei senha, mas a vagas disponíveis já se encerreram o que faço agora ?</li>
+            <li class="lista" onclick="toggleResposta('resposta1')">Qual é o tempo de validade da senha?</li>
+            <div id="resposta1" class="resposta">
+                <p>O tempo de validade da senha é de 15 dias.</p>
+            </div>
+
+            <li class="lista" onclick="toggleResposta('resposta2')">Tirei senha, mas as vagas disponíveis já se encerraram, o que faço agora?</li>
+            <div id="resposta2" class="resposta">
+                <p>Você pode acompanhar o status da sua inscrição pela fila de espera, onde a mesma
+                    irá disponibiliziar posição, validade da senha e status  da inscrição.</p>
+            </div>
+
+            <li class="lista" onclick="toggleResposta('resposta3')">Tirei senha, mas as vagas disponíveis já se encerraram, o que faço agora?</li>
+            <div id="resposta3" class="resposta">
+                <p></p>
+            </div>
         </ul>
     </div>
-    <!-- O restante do conteúdo da página -->
 
+    <script>
+        function toggleResposta(id) {
+            var resposta = document.getElementById(id);
+            resposta.style.display = resposta.style.display === 'none' ? 'block' : 'none';
+        }
+    </script>
 </body>
 
 </html>
