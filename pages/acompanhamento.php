5049ebb095b202967b15500b6950f9ee7b12375a
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-07-05 20:23:03 -0300

    Update acompanhamento.php

--- pages/acompanhamento.php
+++ pages/acompanhamento.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style-table.css">
-    <title>Minha Página</title>
+    <title>Acompanhamento</title>
 </head>
 
 <body>
